325be7e332c64341dddaa9ea98a79ce3386b071b
Sofias2 <sofiacosta5@outlook.com'git config --global user.name Sofiagit config --global user.email sofiacosta5@outlook.comgit config --global user.name "Sofiagit config --global user.email "sofiacosta5@outlook.com>
2022-05-10 12:36:56 -0300

    Atualização

---
+++ Bootcamp Java/Modolo3-/Projeto Java - 3/untitled/src/TiposdeDadoseSintexe.java
@@ -0,0 +1,75 @@
+public class TiposdeDadoseSintexe {
+
+    public static void main(String[] args) {
+
+
+        //tipos de dados//
+        int i1= 1672;
+        int i2 = 2437;
+
+        byte b1=30;
+        byte b2=30;
+
+        short s1=2000;
+        short s2=1000;
+
+        long l1= 20000000000L;
+        long l2= 20000000000L;
+
+        float f1 =2.5F;
+        float f2= 10.25F;
+
+        double d1=2.5;
+
+        char c1='W';
+
+        String st1="OII";
+
+        boolean bl1 =true;
+        boolean bl2=false;
+
+        final int j = 10; // sempre sera 10 //
+
+        //Casting//
+
+        // Downcast - com perda de informação
+
+        byte bi1;
+        short si1 = 1000;
+        bi1 = (byte) si1;
+
+        // Upcast
+        long li1;
+        int ii1 = 10;
+        li1 = ii1;
+
+
+
+
+        System.out.println(i1);
+        System.out.println(j);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(bl1);
+        System.out.println(bl2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(st1);
+        System.out.println(c1);
+        System.out.println(d1);
+        System.out.println(l1);
+        System.out.println(l2);
+
+
+
+
+
+
+    }
+
+
+}
